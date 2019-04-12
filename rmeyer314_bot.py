06d26728ec2d3fc42262d60dbb5781939fc58ce7
Robert Meyer <rhmeyer314@gmail.com>
2019-04-12 10:18:03 -0400

    Update

--- rmeyer314_bot.py
+++ rmeyer314_bot.py
@@ -2,27 +2,26 @@ import tweepy
 import time
 import datetime
 
-CONSUMER_KEY = 'top secret'
-CONSUMER_SECRET = 'top secret'
-ACCESS_KEY = 'top secret'
-ACCESS_SECRET = 'top secret'
+CONSUMER_KEY = 'UcAsfzR52NTlm3vSc35laBaXq'
+CONSUMER_SECRET = 'BMsvAwXDQbxwbpe3J6cg7CR8geoBVbpk0Zs0D0x0nfXRirRP4d'
+ACCESS_KEY = '1108119737974882316-3C1Ez8PixH9rWeHeOHafN5ebQPvB4T'
+ACCESS_SECRET = 'UoePCLFjUauZg9npaQRYGwtUOlV7wfgDQzi9EcHgQbXMK'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-i = 0
-dayNum = 100 # number set to 100 because this program started running on the 101st day of the year
 
 # retrieve current date and print it, along with how many days left in the year
 # and the total percent complete in the yeat
-def update(dayNum):
+def update():
 	currentDT = datetime.datetime.now()
 	month = currentDT.month
 	day = currentDT.day
 	year = currentDT.year
-	totalLeft = 365 - dayNum
-	percent = str(round(dayNum / 365.0, 2))
+	day_of_year = (currentDT - datetime.datetime(currentDT.year, 1, 1)).days + 1
+	totalLeft = 365 - day_of_year
+	percent = str(round(day_of_year / 365.0, 2))
 	api.update_status('Today is ' + str(month) + '/' + str(day) + '/' + str(year) + '\n'
 		'There are ' + str(totalLeft) + ' days left in the year \n'
 		'We are ' + str(percent) + '% complete with ' + str(year) + '\n')
@@ -36,12 +35,8 @@ def favorite():
 			api.create_favorite(statusID.id)
 
 while True:
-	if dayNum == 366: # indicates a new year has started so reset the current day to 0
-		dayNum = 0
-
-	dayNum +=1
 	print('Updating status')
-	update(dayNum)
+	update()
 	#favorite()
 	time.sleep(86400)
 
